81767511f2b9cbcd8f7ba214e62f27484970cb1d
Roman Naumenko <roman.naumenko@ucu.edu.ua>
2024-05-26 11:56:33 +0300

    Reverted temporal debug changes.

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -23,7 +23,7 @@ def format_time(dt):
 # 1. Number of transactions for each cryptocurrency for each hour in the last 6 hours, excluding the previous hour
 @app.get("/reports/transactions/hourly")
 def get_hourly_transactions():
-    end_time = datetime.now(timezone.utc) + timedelta(hours=1)#.replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)  # Exclude the previous hour
+    end_time = datetime.now(timezone.utc).replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)  # Exclude the previous hour
     start_time = end_time - timedelta(hours=6)
     print(f"Looking for transactions between {format_time(start_time)} and {format_time(end_time)}")
     
@@ -44,7 +44,7 @@ def get_hourly_transactions():
 # 2. Total trading volume for each cryptocurrency for the last 6 hours, excluding the previous hour
 @app.get("/reports/volume/last_6_hours")
 def get_volume_last_6_hours():
-    end_time = datetime.now(timezone.utc) + timedelta(hours=1)#.replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)  # Exclude the previous hour
+    end_time = datetime.now(timezone.utc).replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)  # Exclude the previous hour
     start_time = end_time - timedelta(hours=6)
     print(f"Looking for volume between {format_time(start_time)} and {format_time(end_time)}")
     volumes = mongo_db.aggregated_transactions.aggregate([
@@ -73,7 +73,7 @@ def get_volume_last_6_hours():
 # 3. Number of trades and their total volume for each hour in the last 12 hours, excluding the current hour
 @app.get("/reports/trades/last_12_hours")
 def get_trades_last_12_hours():
-    end_time = datetime.now(timezone.utc) + timedelta(hours=1)#.replace(minute=0, second=0, microsecond=0)  # Exclude the current hour
+    end_time = datetime.now(timezone.utc).replace(minute=0, second=0, microsecond=0)  # Exclude the current hour
     start_time = end_time - timedelta(hours=12)
     print(f"Looking for trades between {format_time(start_time)} and {format_time(end_time)}")
     trades = mongo_db.aggregated_transactions.find({
@@ -97,7 +97,7 @@ def get_trades_last_12_hours():
 @app.get("/ad-hoc/transactions/{symbol}/{minutes}")
 def get_trades_for_n_minutes(symbol: str, minutes: int):
     try:
-        current_time = datetime.utcnow()
+        current_time = datetime.now(timezone.utc)
         start_time = current_time - timedelta(minutes=minutes + 1)
         end_time = current_time - timedelta(minutes=1)
 
@@ -119,7 +119,7 @@ def get_trades_for_n_minutes(symbol: str, minutes: int):
 @app.get("/ad-hoc/top-cryptos/{n}")
 def get_top_n_cryptos(n: int):
     try:
-        current_time = datetime.utcnow()
+        current_time = datetime.now(timezone.utc)
         start_time = current_time - timedelta(hours=1)
 
         pipeline = [
